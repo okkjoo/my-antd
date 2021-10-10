70967580cf98be3933a3ad64f398802dc1fa237c
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2021-10-10 10:51:31 +0800

    [chore] storybook 导入全局 css

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,34 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent
+          ? !/node_modules/.test(prop.parent.fileName)
+          : true,
+    },
+  },
+  // webpackFinal: async (config) => {
+  //   config.module.rules.push({
+  //     test: /\.(ts|tsx)$/,
+  //     loader: require.resolve('babel-loader'),
+  //     options: {
+  //       presets: [['react-app', { flow: false, typescript: true }]],
+  //     },
+  //   })
+  //   config.resolve.extensions.push('.ts', '.tsx')
+  //   return config
+  // },
 }
