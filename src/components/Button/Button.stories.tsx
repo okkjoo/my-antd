037bf2d29df849cd21b6c4cad1b27936b58c1aa8
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2021-11-04 16:34:51 +0800

    [refactor] 文件位置

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 // import { withInfo } from '@storybook/addon-info'
 
-import Button from '../components/Button/button'
+import Button from './button'
 
 const styles: React.CSSProperties = {
   textAlign: 'center',
